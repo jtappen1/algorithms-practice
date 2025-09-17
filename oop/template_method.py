88989d80653d37e3a56f3be46ea2738057025fc3
John Tappen <jtappen@gmail.com>
2025-09-16 20:07:09 -0400

    Added a practice template mode problem.

--- oop/template_method.py
+++ oop/template_method.py
@@ -13,7 +13,6 @@ class OrderProcess(ABC):
     def deliver_item(self):
         pass
 
-    @abstractmethod
     def process_order(self):
         self.select_item()
         self.make_payment()
@@ -38,7 +37,7 @@ class PhysicalOrder(OrderProcess):
 # Client code
 def client_code(order: OrderProcess):
     order.process_order()
-    
+
 if __name__ == "__main__":
     print("Digital Order:")
     client_code(DigitalOrder())
@@ -46,3 +45,38 @@ if __name__ == "__main__":
     print("\nPhysical Order:")
     client_code(PhysicalOrder())
     
+########################################################################################################################################################
+
+class RoutePlanner(ABC):
+    def load_map_data(self):
+        print("loading map data")
+
+    def return_route(self):
+        print("returning route")
+
+    @abstractmethod
+    def compute_route(self, route):
+        pass
+
+    @abstractmethod
+    def plan_route(self, route):
+        self.load_map_data()
+        self.compute_route(route)
+        self.return_route()
+    
+
+class FastestRoute(RoutePlanner):
+    pass
+
+    
+
+if __name__ == "__main__":
+    import heapq
+    heap = [2, 3, 1]
+    heapq.heapify(heap)
+    print(heap)
+    print(heapq.heappop(heap))
+    print(heapq.heappush(heap, 0))
+    print(heapq.heappop(heap))
+
+
